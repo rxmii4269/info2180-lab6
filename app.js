af864624e9ef81c4d9a0e6d3628dffd08c8e8289
Romaine Murray <romainemurray6@gmail.com>
2019-11-10 23:38:38 -0500

    Excercise 3 Completed
    Signed-off-by: Romaine Murray <romainemurray6@gmail.com>

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 "use strict";
-window.onload = function () {
 
-    let search = this.document.getElementById("search-btn")
+window.onload = () => {
+    let search = this.document.getElementById("search-btn");
 
-    var xhr = new this.XMLHttpRequest();
-    xhr.open("GET", "superheroes.php", true);
-    xhr.send();
-
+    this.$("#searchform").submit(function (e) {
+        e.preventDefault();
+    });
 
     search.addEventListener("click", () => {
-
-        this.alert(xhr.responseText);
+        this.$.ajax({
+            type: "GET",
+            url: "superheroes.php",
+            data: this.$("#searchform").serialize(),
+            success: function (data) {
+                let result = document.getElementById("result");
+                result.innerHTML = data;
+            },
+            dataType: "html"
+        });
     });
 };
